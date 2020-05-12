93527d7428cdeb6aaba26d4045d99cfffc45fb51
Russ Williams <ruwill@microsoft.com>
2020-05-11 23:12:33 -0400

    Updated the embedding snippet for the web chat control and the TokenController to include the generation of UserId that support enhanced security

--- SecuredWebChatControl/Controllers/TokenController.cs
+++ SecuredWebChatControl/Controllers/TokenController.cs
@@ -11,16 +11,17 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 /* ----------------------------------------------------------------------------------
- * Important!!!
+ * ToDo: Important tasks that must be complete for TokenController will work
  * The following tasks must be completed before running this code or it will fail:
- *  * 1) To avoid connection exhaustion issues, this code exercises the best practice of
- *    only using a single HttpClient instance per application so you'll need to add 
- *    dependency injection by adding the follwing line of code to the Startup.cs file 
- *    somewhere in the ConfigureServices() method:
- *          services.AddSingleton<HttpClient>(new HttpClient());
- * 2) This code expects that to get the Bot Secret from the appsettings.json file so 
+ * 1) To avoid connection exhaustion issues, this code uses a single static instance
+ *    of an HttpClient that needs to be added as a member of the Startup class:
+ *    `public static HttpClient HttpClient = new HttpClient();`
+ * 2) Change the fully qualified namespace of the reference to the HttpClient on line 42 
+ *    of the TokenController constructor to match the namespace of your Startup class:
+ *    `Client = <Your Namespace Here>.Startup.HttpClient;`
+ * 2) This code expects that to get the DirectLine secret from the appsettings.json file so 
  *    you'll need to add the following setting to the appsettings.json file:
- *          "directLineSecret": "<YourDirectLineSecret>",
+ *          `"directLineSecret": "<YourDirectLineSecret>",`
  * 3) Go to the Azure portal and enable the Direct Line channel and copy the Secret Key
  *    and paste it in as the value of the directLineSecret setting that you created in
  *    the previous step.
@@ -35,46 +36,59 @@ namespace SecuredWebChatConrol.Controllers
         public IConfiguration Configuration { get; set; }
         public HttpClient Client { get; set; }
 
-        public TokenController(IConfiguration configuration, HttpClient httpClient)
+        public TokenController(IConfiguration configuration)
         {
             // If you are seeing a runtime error that complains about not being able to inject the
             // HttpClient, then see the note at the note at the top of this file
             Configuration = configuration;
-            Client = httpClient;
+            Client = Microsoft.BotBuilderSamples.Startup.HttpClient;
         }
 
         // GET: api/Token
         [HttpGet]
+        [Produces("application/json")]
         async public Task<string> Get()
         {
             string generateTokenUri = "https://directline.botframework.com/v3/directline/tokens/generate";
             var directLineSecret = Configuration["directLineSecret"];
 
-            return await GetTokenAsync(generateTokenUri, directLineSecret);
+            return JsonConvert.SerializeObject(await GetTokenAsync(generateTokenUri, directLineSecret));
         }
 
         // GET: api/Token/5
         [HttpGet("{token}", Name = "Get")]
-        async public Task<string> Get(string token)
+        [Produces("application/json")]
+        async public Task<string> Get(string directLineSecret)
         {
-            return await GetTokenAsync("https://directline.botframework.com/v3/directline/tokens/refresh", token);
+            return JsonConvert.SerializeObject(await GetTokenAsync("https://directline.botframework.com/v3/directline/tokens/refresh", directLineSecret));
         }
 
-        async Task<string> GetTokenAsync(string api, string bearerValue)
+        async Task<object> GetTokenAsync(string api, string directLineSecret)
         {
+            dynamic response = new JObject();
+
+            // Initialize the response object
+            response.Token = "";
+            response.UserId = "";
+            response.IsSuccess = false;
+
+            // Swap DirectLine secret for access token
             Client.DefaultRequestHeaders.Remove("Authorization");
-            Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerValue}");
+            Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {directLineSecret}");
 
-            HttpResponseMessage response = await Client.PostAsync(api, null);
+            HttpResponseMessage directLineResponse = await Client.PostAsync(api, null);
 
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            response.IsSuccess = directLineResponse.IsSuccessStatusCode;
+
+            if ((bool)response.IsSuccess)
             {
-                string body = response.Content.ReadAsStringAsync().Result;
+                string body = directLineResponse.Content.ReadAsStringAsync().Result;
 
-                return (string) JObject.Parse(body)["token"];
+                response.Token = (string)JObject.Parse(body)["token"];
+                response.UserId = $"dl_{Guid.NewGuid()}";
             }
-            else
-                return null;
+
+            return response;
         }
     }
 }
